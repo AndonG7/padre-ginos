9022aaea163f117f02aeb1f3e0b982bf2862c591
Andon Gjorgjiev <gjorgjiev.andon@uklo.edu.mk>
2025-02-19 00:28:07 +0100

    11 lessons completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+import Pizza from "./Pizza";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Padre Gino's - Order Now</h1>
+      <Pizza name="Pepperoni" description="pep, cheese, n stuff" image={"/public/pizzas/pepperoni.webp"} />
+      <Pizza name="Capriciossa" description="ham, cheese, n stuff" image={"/public/pizzas/hawaiian.webp"} />
+      <Pizza name="Americano" description="french fries, hot dogs" image={"/public/pizzas/big_meat.webp"} />
+    </div>
+  )
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
