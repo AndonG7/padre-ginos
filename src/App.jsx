13a069487e37e78192504108715067547d73d179
Andon Gjorgjiev <gjorgjiev.andon@uklo.edu.mk>
2025-02-21 00:47:11 +0100

    started using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni" description="pep, cheese, n stuff" image={"/public/pizzas/pepperoni.webp"} />
-      <Pizza name="Capriciossa" description="ham, cheese, n stuff" image={"/public/pizzas/hawaiian.webp"} />
-      <Pizza name="Americano" description="french fries, hot dogs" image={"/public/pizzas/big_meat.webp"} />
-    </div>
+      <Order />
+     </div>
   )
 };
 
